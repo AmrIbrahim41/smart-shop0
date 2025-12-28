25d8793355208138fddd35c5e018ea7c4eb227fb
AmrIbrahim41 <Amrhima41@gmail.com>
2025-12-29 01:49:38 +0200

    final isa

--- Front-End/src/api.js
+++ Front-End/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-/// لو مشروعك معمول بـ Vite (وده الغالب دلوقتي)
+
 const BASE_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:8000/";
 
 const api = axios.create({
